d6319950a8781fa256e0e630b61610cc0c4ebfcb
Nathaniel Schier <nathaniel.schier@gmail.com>
2017-12-04 12:30:31 -0800

    Initial commit. Add base project files.

---
+++ bot.js
@@ -0,0 +1,4 @@
+const twit = require('twit');
+const config = require('./config.js');
+
+var Twitter = new twit(config);
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: 'oIKn0c8BKxE03pbhj2L3JyKMN',  
+  consumer_secret: 'qJSe0QqUkCatBLNxOfQlDIgiseImKd1ntKo2uYbFRFsWBU3ORK',
+  access_token: '937776056869597185-8bpnCyVmzuOhN51DvPcQkBTrWcw1ZSh',  
+  access_token_secret: '87ul8Df0d0gZU0GwC9CQTwxV1uhCCue6MSwK92HhxFDHE'
+}
